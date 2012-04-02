042a093d54e1e0bf94012de1ec2835e89b4326eb
Joakim Ekberg <jocke.ekberg@gmail.com>
2012-04-02 09:25:31 +0200

    Group HTTP_ACCEPT_LANGUAGE specs together.

--- spec/lib/rack/locale_spec.rb
+++ spec/lib/rack/locale_spec.rb
@@ -8,42 +8,46 @@ def app
 end
 
 describe Rack::Locale do
-  before do
+  describe "" do
+    before do
+    end
+
+    xit "should set the REMOTE_ADDR header" do
+      get '/', {}, {"REMOTE_ADDR" => "10.0.0.1"}
+      last_request.env["locale.country"].should == ["us"]
+      last_request.ip.should == "10.0.0.1"
+    end
   end
 
-  xit "should set the REMOTE_ADDR header" do
-    get '/', {}, {"REMOTE_ADDR" => "10.0.0.1", "HTTP_ACCEPT_LANGUAGE" => "en"}
-    last_request.env["locale.languages"].should == ["en"]
-    last_request.ip.should == "10.0.0.1"
-  end
+  describe "parsing HTTP_ACCEPT_LANGUAGE" do
+    it "should parse HTTP_ACCEPT_LANGUAGE 'en'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en"}
+      last_request.env["locale.languages"].should == ["en"]
+    end
 
-  it "should parse HTTP_ACCEPT_LANGUAGE 'en'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en"}
-    last_request.env["locale.languages"].should == ["en"]
-  end
+    it "should parse HTTP_ACCEPT_LANGUAGE 'sv'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv"}
+      last_request.env["locale.languages"].should == ["sv"]
+    end
 
-  it "should parse HTTP_ACCEPT_LANGUAGE 'sv'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv"}
-    last_request.env["locale.languages"].should == ["sv"]
-  end
+    it "should parse HTTP_ACCEPT_LANGUAGE 'sv;q=0.1, en'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv;q=0.1, en"}
+      last_request.env["locale.languages"].should == ["en", "sv"]
+    end
 
-  it "should parse HTTP_ACCEPT_LANGUAGE 'sv;q=0.1, en'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv;q=0.1, en"}
-    last_request.env["locale.languages"].should == ["en", "sv"]
-  end
+    it "should parse HTTP_ACCEPT_LANGUAGE 'sv, en'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv, en"}
+      last_request.env["locale.languages"].should == ["sv", "en"]
+    end
 
-  it "should parse HTTP_ACCEPT_LANGUAGE 'sv, en'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "sv, en"}
-    last_request.env["locale.languages"].should == ["sv", "en"]
-  end
+    it "should parse HTTP_ACCEPT_LANGUAGE 'en;q=0.4, de;q=0.7'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en;q=0.4, de;q=0.7"}
+      last_request.env["locale.languages"].should == ["de", "en"]
+    end
 
-  it "should parse HTTP_ACCEPT_LANGUAGE 'en;q=0.4, de;q=0.7'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en;q=0.4, de;q=0.7"}
-    last_request.env["locale.languages"].should == ["de", "en"]
-  end
-
-  it "should parse HTTP_ACCEPT_LANGUAGE 'en-US;q=0.7'" do
-    get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en-US;q=0.7"}
-    last_request.env["locale.languages"].should == ["en"]
+    it "should parse HTTP_ACCEPT_LANGUAGE 'en-US;q=0.7'" do
+      get '/', {}, {"HTTP_ACCEPT_LANGUAGE" => "en-US;q=0.7"}
+      last_request.env["locale.languages"].should == ["en"]
+    end
   end
 end
